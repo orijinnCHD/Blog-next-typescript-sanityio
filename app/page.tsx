cee406538c08e558b5046d0687e87c29c1d68550
orijinnCHD <reussir.startup@gmail.com>
2023-02-01 15:40:21 +0400

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage(){
+    return(
+        <div>
+           <h1 className="text-4xl">welcome to the blog</h1>
+            <p>some content</p>
+        </div>
+    )
+}
